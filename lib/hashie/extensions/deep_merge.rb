5d42698b4be1e06d2ba3c009c06f4fce9e0317e6
bartoszkopinski <bartosz.kopinski@netguru.pl>
2014-05-24 19:53:22 +0200

    DeepMerge refactored to work with other extensions

--- lib/hashie/extensions/deep_merge.rb
+++ lib/hashie/extensions/deep_merge.rb
@@ -2,28 +2,33 @@ module Hashie
   module Extensions
     module DeepMerge
       # Returns a new hash with +self+ and +other_hash+ merged recursively.
-      def deep_merge(other_hash)
-        dup.deep_merge!(other_hash)
+      def deep_merge(other_hash, &block)
+        dup.deep_merge!(other_hash, &block)
       end
 
       # Returns a new hash with +self+ and +other_hash+ merged recursively.
       # Modifies the receiver in place.
-      def deep_merge!(other_hash)
-        _recursive_merge(self, other_hash)
+      def deep_merge!(other_hash, &block)
+        _recursive_merge(self, other_hash, &block)
         self
       end
 
       private
 
-      def _recursive_merge(hash, other_hash)
-        if other_hash.is_a?(::Hash) && hash.is_a?(::Hash)
-          other_hash.each do |k, v|
-            hash[k] = hash.key?(k) ? _recursive_merge(hash[k], v) : v
+      def _recursive_merge(hash, other_hash, &block)
+        return other_hash if !other_hash.is_a?(::Hash)
+        other_hash.each_pair do |k, v|
+          tv = hash[k]
+          hash[k] = case tv
+          when self.class, Hash
+            _recursive_merge tv, v, &block
+          when ::Hash
+            _recursive_merge self.class[tv], v, &block
+          else
+            block_given? ? block.call(k, tv, v) : v
           end
-          hash
-        else
-          other_hash
         end
+        hash
       end
     end
   end
