73a8484ee876b0aa0ff233fd85e1890d6645e28a
bartoszkopinski <bartosz.kopinski@netguru.pl>
2014-05-24 20:16:22 +0200

    Extending MethodAccess functionality from Mash
    Compatibility change: does not raise exceptions for undefined keys

--- lib/hashie/extensions/method_access.rb
+++ lib/hashie/extensions/method_access.rb
@@ -1,123 +1,63 @@
 module Hashie
   module Extensions
-    # MethodReader allows you to access keys of the hash
-    # via method calls. This gives you an OStruct like way
-    # to access your hash's keys. It will recognize keys
-    # either as strings or symbols.
-    #
-    # Note that while nil keys will be returned as nil,
-    # undefined keys will raise NoMethodErrors. Also note that
-    # #respond_to? has been patched to appropriately recognize
-    # key methods.
-    #
-    # @example
-    #   class User < Hash
-    #     include Hashie::Extensions::MethodReader
-    #   end
-    #
-    #   user = User.new
-    #   user['first_name'] = 'Michael'
-    #   user.first_name # => 'Michael'
-    #
-    #   user[:last_name] = 'Bleigh'
-    #   user.last_name # => 'Bleigh'
-    #
-    #   user[:birthday] = nil
-    #   user.birthday # => nil
-    #
-    #   user.not_declared # => NoMethodError
-    module MethodReader
-      def respond_to?(name, include_private = false)
-        return true if key?(name.to_s) || key?(name.to_sym)
-        super
-      end
-
-      def method_missing(name, *args)
-        return self[name.to_s] if key?(name.to_s)
-        return self[name.to_sym] if key?(name.to_sym)
-        super
-      end
-    end
-
-    # MethodWriter gives you #key_name= shortcuts for
-    # writing to your hash. Keys are written as strings,
-    # override #convert_key if you would like to have symbols
-    # or something else.
-    #
-    # Note that MethodWriter also overrides #respond_to such
-    # that any #method_name= will respond appropriately as true.
-    #
-    # @example
-    #   class MyHash < Hash
-    #     include Hashie::Extensions::MethodWriter
-    #   end
-    #
-    #   h = MyHash.new
-    #   h.awesome = 'sauce'
-    #   h['awesome'] # => 'sauce'
-    #
-    module MethodWriter
-      def respond_to?(name, include_private = false)
-        return true if name.to_s =~ /=$/
-        super
-      end
-
-      def method_missing(name, *args)
-        if args.size == 1 && name.to_s =~ /(.*)=$/
-          return self[convert_key(Regexp.last_match[1])] = args.first
-        end
-
-        super
-      end
-
-      def convert_key(key)
-        key.to_s
-      end
-    end
-
-    # MethodQuery gives you the ability to check for the truthiness
-    # of a key via method calls. Note that it will return false if
-    # the key is set to a non-truthful value, not if the key isn't
-    # set at all. Use #key? for checking if a key has been set.
-    #
-    # MethodQuery will check against both string and symbol names
-    # of the method for existing keys. It also patches #respond_to
-    # to appropriately detect the query methods.
-    #
-    # @example
-    #   class MyHash < Hash
-    #     include Hashie::Extensions::MethodQuery
-    #   end
-    #
-    #   h = MyHash.new
-    #   h['abc'] = 123
-    #   h.abc? # => true
-    #   h['def'] = nil
-    #   h.def? # => false
-    #   h.hji? # => NoMethodError
-    module MethodQuery
-      def respond_to?(name, include_private = false)
-        return true if name.to_s =~ /(.*)\?$/ && (key?(Regexp.last_match[1]) || key?(Regexp.last_match[1].to_sym))
-        super
-      end
-
-      def method_missing(name, *args)
-        if args.empty? && name.to_s =~ /(.*)\?$/ && (key?(Regexp.last_match[1]) || key?(Regexp.last_match[1].to_sym))
-          return self[Regexp.last_match[1]] || self[Regexp.last_match[1].to_sym]
-        end
-
-        super
-      end
-    end
-
-    # A macro module that will automatically include MethodReader,
-    # MethodWriter, and MethodQuery, giving you the ability to read,
-    # write, and query keys in a hash using method call shortcuts.
     module MethodAccess
+      ALLOWED_SUFFIXES = %w(? ! = _)
+      SPLIT_REGEXP     = /([#{ALLOWED_SUFFIXES.join}])$/
+
       def self.included(base)
-        [MethodReader, MethodWriter, MethodQuery].each do |mod|
-          base.send :include, mod
+        base.send :include, PrettyInspect
+        base.send :include, IndifferentAccess
+      end
+
+      def prefix_method?(method_name)
+        method_name = method_name.to_s
+        method_name.end_with?(*ALLOWED_SUFFIXES) && key?(method_name.chop)
+      end
+
+      def method_missing(method_name, *args, &blk)
+        return self.[](method_name, &blk) if key?(method_name)
+        name, suffix = method_suffix(method_name)
+        case suffix
+        when '='
+          self[name] = args.first
+        when '?'
+          !!self[name]
+        when '!'
+          initializing_reader(name)
+        when '_'
+          underbang_reader(name)
+        else
+          default(method_name)
         end
+      end
+
+      def respond_to_missing?(method_name, *args)
+        return true if key?(method_name)
+        _, suffix = method_suffix(method_name)
+        suffix.nil? ? super : true
+      end
+
+      # This is the bang method reader, it will return a new Mash
+      # if there isn't a value already assigned to the key requested.
+      def initializing_reader(key)
+        self[key] = self.class.new unless key?(key)
+        self[key]
+      end
+
+      # This is the under bang method reader, it will return a temporary new Mash
+      # if there isn't a value already assigned to the key requested.
+      def underbang_reader(key)
+        if key?(key)
+          self[key]
+        else
+          self.class.new
+        end
+      end
+
+      protected
+
+      def method_suffix(method_name)
+        method_name.to_s.split(SPLIT_REGEXP)
       end
     end
   end
--- spec/hashie/extensions/method_access_spec.rb
+++ spec/hashie/extensions/method_access_spec.rb
@@ -1,32 +1,34 @@
 require 'spec_helper'
 
-describe Hashie::Extensions::MethodReader do
-  class ReaderHash < Hash
-    include Hashie::Extensions::MethodReader
+describe Hashie::Extensions::MethodAccess do
+  class MethodHash < Hash
+    include Hashie::Extensions::MethodAccess
 
     def initialize(hash = {})
       update(hash)
     end
   end
 
-  subject { ReaderHash }
+  subject { MethodHash }
 
-  it 'reads string keys from the method' do
-    expect(subject.new('awesome' => 'sauce').awesome).to eq 'sauce'
-  end
+  context 'reader' do
+    it 'reads string keys from the method' do
+      expect(subject.new('awesome' => 'sauce').awesome).to eq 'sauce'
+    end
 
-  it 'reads symbol keys from the method' do
-    expect(subject.new(awesome: 'sauce').awesome).to eq 'sauce'
-  end
+    it 'reads symbol keys from the method' do
+      expect(subject.new(awesome: 'sauce').awesome).to eq 'sauce'
+    end
 
-  it 'reads nil and false values out properly' do
-    h = subject.new(nil: nil, false: false)
-    expect(h.nil).to eq nil
-    expect(h.false).to eq false
-  end
+    it 'reads nil and false values out properly' do
+      h = subject.new(nil: nil, false: false)
+      expect(h.nil).to eq nil
+      expect(h.false).to eq false
+    end
 
-  it 'raises a NoMethodError for undefined keys' do
-    expect { subject.new.awesome }.to raise_error(NoMethodError)
+    it 'raises a NoMethodError for undefined keys' do
+      expect { subject.new.awesome }.to raise_error(NoMethodError)
+    end
   end
 
   describe '#respond_to?' do
@@ -42,80 +44,68 @@ describe Hashie::Extensions::MethodReader do
       expect(subject.new).not_to be_respond_to(:awesome)
     end
   end
-end
 
-describe Hashie::Extensions::MethodWriter do
-  class WriterHash < Hash
-    include Hashie::Extensions::MethodWriter
-  end
+  context 'writer' do
+    subject{ MethodHash.new }
 
-  subject { WriterHash.new }
+    it 'writes from a method call' do
+      subject.awesome = 'sauce'
+      expect(subject['awesome']).to eq 'sauce'
+    end
 
-  it 'writes from a method call' do
-    subject.awesome = 'sauce'
-    expect(subject['awesome']).to eq 'sauce'
-  end
+    it 'writes from a method call' do
+      subject.awesome = { type: 'sauce' }
+      expect(subject['awesome']['type']).to eq 'sauce'
+    end
 
-  it 'converts the key using the #convert_key method' do
-    allow(subject).to receive(:convert_key).and_return(:awesome)
-    subject.awesome = 'sauce'
-    expect(subject[:awesome]).to eq 'sauce'
-  end
+    it 'writes from a method call', focus: true do
+      subject.awesome = { type: 'sauce' }
+      expect(subject.awesome.type).to eq 'sauce'
+    end
 
-  it 'raises NoMethodError on non equals-ending methods' do
-    expect { subject.awesome }.to raise_error(NoMethodError)
-  end
+    it 'converts the key using the #convert_key method' do
+      allow(subject).to receive(:convert_key).and_return(:awesome)
+      subject.awesome = 'sauce'
+      expect(subject[:awesome]).to eq 'sauce'
+    end
 
-  it '#respond_to? correctly' do
-    expect(subject).to be_respond_to(:abc=)
-    expect(subject).not_to be_respond_to(:abc)
-  end
-end
+    it 'raises NoMethodError on non equals-ending methods' do
+      expect { subject.awesome }.to raise_error(NoMethodError)
+    end
 
-describe Hashie::Extensions::MethodQuery do
-  class QueryHash < Hash
-    include Hashie::Extensions::MethodQuery
-
-    def initialize(hash = {})
-      update(hash)
+    it '#respond_to? correctly' do
+      expect(subject).to be_respond_to(:abc=)
+      expect(subject).not_to be_respond_to(:abc)
     end
   end
 
-  subject { QueryHash }
+  context 'predicate' do
+    it 'is true for non-nil string key values' do
+      expect(subject.new('abc' => 123)).to be_abc
+    end
 
-  it 'is true for non-nil string key values' do
-    expect(subject.new('abc' => 123)).to be_abc
-  end
+    it 'is true for non-nil symbol key values' do
+      expect(subject.new(abc: 123)).to be_abc
+    end
 
-  it 'is true for non-nil symbol key values' do
-    expect(subject.new(abc: 123)).to be_abc
-  end
+    it 'is false for nil key values' do
+      expect(subject.new(abc: false)).not_to be_abc
+    end
 
-  it 'is false for nil key values' do
-    expect(subject.new(abc: false)).not_to be_abc
-  end
+    it 'raises a NoMethodError for non-set keys' do
+      expect { subject.new.abc? }.to raise_error(NoMethodError)
+    end
 
-  it 'raises a NoMethodError for non-set keys' do
-    expect { subject.new.abc? }.to raise_error(NoMethodError)
-  end
+    it '#respond_to? for existing string keys' do
+      expect(subject.new('abc' => 'def')).to be_respond_to('abc?')
+    end
 
-  it '#respond_to? for existing string keys' do
-    expect(subject.new('abc' => 'def')).to be_respond_to('abc?')
-  end
+    it '#respond_to? for existing symbol keys' do
+      expect(subject.new(abc: 'def')).to be_respond_to(:abc?)
+    end
 
-  it '#respond_to? for existing symbol keys' do
-    expect(subject.new(abc: 'def')).to be_respond_to(:abc?)
-  end
-
-  it 'does not #respond_to? for non-existent keys' do
-    expect(subject.new).not_to be_respond_to('abc?')
-  end
-end
-
-describe Hashie::Extensions::MethodAccess do
-  it 'includes all of the other method mixins' do
-    klass = Class.new(Hash)
-    klass.send :include, Hashie::Extensions::MethodAccess
-    expect((klass.ancestors & [Hashie::Extensions::MethodReader, Hashie::Extensions::MethodWriter, Hashie::Extensions::MethodQuery]).size).to eq 3
+    it 'does not #respond_to? for non-existent keys' do
+      expect(subject.new).not_to be_respond_to('abc?')
+    end
   end
 end
